3daf19cca8802cde4a45b52980de45b577a8c55d
Roman13456 <romankhromishin@gmail.com>
2022-08-11 12:56:13 +0300

    quick fix to order_received page

--- js/order_received.js
+++ js/order_received.js
@@ -1,9 +1,24 @@
-const date = new Date()
-const orderNumberStr = new Date().getTime()
-const paymentMethodStr = localStorage.getItem("paymentMethod")
-const orderNumber = document.querySelector(".orderNumber")
-const currentDate = document.querySelector(".currentDate")
-const paymentMethod = document.querySelector(".paymentMethod")
-orderNumber.innerHTML = orderNumberStr
-currentDate.innerHTML = `${new Date().toLocaleString(['en-us'], { month: 'long' })} ${new Date().getDate()}, ${new Date().getFullYear()}`
-paymentMethod.innerHTML = paymentMethodStr
+const date = new Date();
+const orderNumberStr = new Date().getTime();
+const paymentMethodStr = localStorage.getItem("paymentMethod");
+const orderNumber = document.querySelector(".orderNumber");
+const currentDate = document.querySelector(".currentDate");
+const paymentMethod = document.querySelector(".paymentMethod");
+if (localStorage.getItem("order_received") !== null) {
+    localStorage.removeItem("order_received");
+    console.log('h')
+    window.open('index.html', '_self')
+} else {
+    localStorage.setItem("order_received", "true");
+    orderNumber.innerHTML = orderNumberStr;
+    currentDate.innerHTML = `${new Date().toLocaleString(["en-us"], {
+        month: "long",
+    })} ${new Date().getDate()}, ${new Date().getFullYear()}`;
+    paymentMethod.innerHTML = paymentMethodStr;
+    let productsArray;
+    fetch("https://62d575ef15ad24cbf2c7a034.mockapi.io/products")
+        .then((res) => res.json())
+        .then((products) => {
+            productsArray = products;
+        });
+}
