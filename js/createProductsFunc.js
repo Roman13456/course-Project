9bfca19e8f586d1f2018ae7b69b57bb1d0298ad0
Roman13456 <romankhromishin@gmail.com>
2022-09-06 14:31:18 +0300

    add good-looking hover anim on products+some minor changes

--- js/createProductsFunc.js
+++ js/createProductsFunc.js
@@ -3,28 +3,53 @@ function createProductClosure(loc,lg,xxl){
     function createProduct(element){
         place.insertAdjacentHTML("beforeend",`
         <div class="productItemLink col-6 col-lg-${lg} col-xxl-${xxl} position-relative" data-id='${element.id}'>
-            <a href="products_page.html" class=" d-block">
-                <img src="${element.imgsGallery[0]}" alt="printed blazer">
-            </a>
-            ${(()=>{
-                if(element.isSale){
-                    return `<p class="saleIndicator position-absolute">SALE</p>`
-                }else{
-                    return ""
-                 }
-            })()}
-            <a class="d-block" href="products_page.html">${element.name}</a>
-            <p class='itemPrice'>$${element.price}
+            <div class='usualProductItem'>
+                <a href="products_page.html" class=" d-block">
+                    <img src="${element.imgsGallery[0]}" alt="printed blazer">
+                </a>
                 ${(()=>{
                     if(element.isSale){
-                        return `<span>$${element.oldPrice}</span>`
+                        return `<p class="saleIndicator position-absolute">SALE</p>`
                     }else{
-                        return ''
+                        return ""
                     }
                 })()}
-            </p>
-            <button data-id ='${element.id}' class='buttonEvent'>Add to cart</button>
+                <a class="d-block" href="products_page.html">${element.name}</a>
+                <p class='itemPrice'>$${element.price}
+                    ${(()=>{
+                        if(element.isSale){
+                            return `<span>$${element.oldPrice}</span>`
+                        }else{
+                            return ''
+                        }
+                    })()}
+                </p>
+                <button data-id ='${element.id}' class='buttonEvent'>Add to cart</button>
             </div>
+            <div class='hoverProductItem'>
+                <a href="products_page.html" class=" d-block">
+                    <img src="${element.imgsGallery[0]}" alt="printed blazer">
+                </a>
+                ${(()=>{
+                    if(element.isSale){
+                        return `<p class="saleIndicator position-absolute">SALE</p>`
+                    }else{
+                        return ""
+                    }
+                })()}
+                <a class="d-block" href="products_page.html">${element.name}</a>
+                <p class='itemPrice'>$${element.price}
+                    ${(()=>{
+                        if(element.isSale){
+                            return `<span>$${element.oldPrice}</span>`
+                        }else{
+                            return ''
+                        }
+                    })()}
+                </p>
+                <button data-id ='${element.id}' class='buttonEvent'>Add to cart</button>
+            </div>
+        </div>
         `)
         const buttons = Array.from(document.querySelectorAll('.productItemLink button')) 
         const exactBtn = buttons.find((e)=>e.getAttribute('data-id')==element.id)
